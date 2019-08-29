37829a1958159d3d765bc3c3d32302459af8eb7a
Leonardo Brito <lbrito@gmail.com>
2019-08-29 20:48:03 -0300

    Creates personal blog deployment script.

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # Compile output pages/assets
-git checkout development
+git checkout blog-dev
 nanoc
 
 # Temporarily stash changes
 git stash
 
 # Switch to Github Pages branch
-git checkout master
+git checkout blog-master
 git rm *
-git checkout development -- output/
+git checkout blog-dev
 git add .
 git commit -m "Builds blog. (automated commit)"
 
 # Push & deploy
-git push origin master
+git push blog/master blog-master
 
 # Pop files
-git checkout development
+git checkout blog-dev
 git stash pop
