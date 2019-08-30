23a22a7214dae3f9ffe669a0409aee0969a50ee3
Leonardo Brito <lbrito@gmail.com>
2019-08-29 21:18:45 -0300

    Updates deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
+# These variables are useful in case you use a different repo for different branches
+# E.g., I do that to push local branches to my personal blog's repo, which is
+# different from this one.
+local_development_branch="blog-dev"
+local_master_branch="blog-master"
+remote="blog"
+
 # Make sure we're in the correct dir
-git checkout blog-dev
+git checkout $local_development_branch
 
 # Update remote
 git add .
 git commit -m "Updates development branch. (automated commit)"
-git push blog blog-dev:development
+git push $remote $local_development_branch:development
 
 # Compile output pages/assets
 nanoc
 
 # Switch to Github Pages branch
-git checkout blog-master
+git checkout $local_master_branch
 git rm * -r
 
 # Copy nanoc output to root
-git checkout blog-dev output/
+git checkout $local_development_branch output/ README.md
 cp -r output/* ./
 rm -rf ./output/*
 
@@ -25,7 +32,7 @@ git add .
 git commit -m "Builds blog. (automated commit)"
 
 # Push & deploy
-git push blog blog-master:master
+git push $remote $local_master_branch:master
 
 # Pop files
-git checkout blog-dev
+git checkout $local_development_branch
