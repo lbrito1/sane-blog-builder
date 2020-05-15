19ba3e50bb31c84dcf8b6c51a2ed3eb5f1137f49
Leonardo Brito <lbrito@gmail.com>
2020-05-15 18:35:30 -0300

    Improves deploy script.

--- deploy.sh
+++ deploy.sh
@@ -3,20 +3,21 @@
 # These variables are useful in case you use a different repo for different branches
 # E.g., I do that to push local branches to my personal blog's repo, which is
 # different from this one.
-local_development_branch="blog-dev"
-local_master_branch="blog-master"
-remote="blog"
+local_development_branch="development"
+local_master_branch="master"
+remote="origin"
 
 # Make sure we're in the correct dir
 git checkout $local_development_branch
+
+# Compile output pages/assets
+ENV="production" # necessary for assets in production
+nanoc
 
 # Update remote
 git add .
 git commit -m "Updates development branch. (automated commit)"
 git push $remote $local_development_branch:development
-
-# Compile output pages/assets
-nanoc
 
 # Switch to Github Pages branch
 git checkout $local_master_branch
@@ -25,7 +26,7 @@ git rm * -r
 # Copy nanoc output to root
 git checkout $local_development_branch output/ README.md
 cp -r output/* ./
-rm -rf ./output/*
+rm -rf ./output ./crash.log ./deploy.sh
 
 # Send changes to remote (production branch)
 git add .
